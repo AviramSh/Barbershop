e323226f0bf0e9ae7e8d39e28de2ee8b5d472b25
Aviram Sharabi <aviram@sharabi.co.il>
2017-03-26 22:58:40 +0300

    Creating Initial Activity 1.1

---
+++ app/src/main/java/com/esh_tech/aviram/barbershop/StockActivity.java
@@ -0,0 +1,13 @@
+package com.esh_tech.aviram.barbershop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock);
+    }
+}
